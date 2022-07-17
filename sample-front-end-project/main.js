5d78594526ecf2f321a3ba5ca1046296d5a3b93d
Mohamed Talaat Harb <mohamed.harb@cegedim.com>
2022-07-17 18:11:59 +0200

    adding a sample front-end project

---
+++ sample-front-end-project/main.js
@@ -0,0 +1,16 @@
+const cardImg = document.querySelector(".card-img");
+const cardName = document.querySelector(".card-name");
+const cardInfo = document.querySelector(".card-info");
+
+const cardData = [
+  { name: "M. Talaat Harb", img: "assets/mharb.webp", info: "TL of Cairo 6 from the Maiia Gestion team" },
+  
+];
+
+function updateCard(id) {
+  const selectedData = cardData[id];
+
+  cardImg.src = selectedData.img;
+  cardName.innerHTML = selectedData.name;
+  cardInfo.innerHTML = selectedData.info;
+}
