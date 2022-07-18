c0a607c70af7b3c3ac617003e961932852d93580
something <some@thing.com>
2022-07-18 06:45:09 -0700

    mostafa's commit

--- sample-front-end-project/main.js
+++ sample-front-end-project/main.js
@@ -12,7 +12,7 @@ const cardData = [
   { name: 'Almoatasem Belah', img: 'assets/mharb.webp', info: 'QA intern' },
   //heiii
   { name: 'Hashem Yehia', img: 'assets/mharb.webp', info: 'Computer Science student' },
-  { name: 'MostafaEsmail3'
+  { name: 'MostafaEsmail5'
   , img: 'https://imgs.search.brave.com/uWySc1aj3JGNM8ymHEb8zjZMKRVBVQDG303z39YseLs/rs:fit:632:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5J/eDZYak1idUN2b3Ez/RVFOZ0pveUVRSGFG/aiZwaWQ9QXBp'
   , info: 'Someone else' }
   // ehhhhhh
