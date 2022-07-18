162f60cc233aac2bccc6e783eba5451c0041a5f6
Almoatasem <elmoatasemahmed@gmail.com>
2022-07-18 16:43:44 +0200

    hi world

--- sample-front-end-project/main.js
+++ sample-front-end-project/main.js
@@ -4,17 +4,12 @@ const cardInfo = document.querySelector(".card-info");
 
 const cardData = [
   { name: "M. Talaat Harb", img: "assets/mharb.webp", info: "TL of Cairo 6 from the Maiia Gestion team" },
-  { name: "Almoatasem Belah ", img: "assets/mharb.webp", info: "QA intern at Cegedim" },
-  
-
   {
     name: 'M. Talaat Harb',
     img: 'assets/mharb.webp',
     info: 'TL of Cairo 6 from the Maiia Gestion team',
   },
   { name: 'Mohamed Gado', img: 'assets/mharb.webp', info: 'Sadge' },
-  { name: 'Almoatasem Belah', img: 'assets/mharb.webp', info: 'QA intern' },
-  //heiii
   {
     name: 'Hashem Yehia',
     img: 'assets/mharb.webp',
