e78905fc50ec904856d333ce2b4a7382e61c88c7
Amr <amressam17@hotmail.com>
2022-07-18 06:50:58 -0700

    amr'spush

--- sample-front-end-project/main.js
+++ sample-front-end-project/main.js
@@ -21,6 +21,12 @@ const cardData = [
     img: 'https://imgs.search.brave.com/uWySc1aj3JGNM8ymHEb8zjZMKRVBVQDG303z39YseLs/rs:fit:632:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5J/eDZYak1idUN2b3Ez/RVFOZ0pveUVRSGFG/aiZwaWQ9QXBp',
     info: 'Someone else',
   },
+
+  {
+    name: 'Amr Essam',
+    img: 'assets/mharb.webp',
+    info: 'Hello',
+  },
   // ehhhhhh
 ]
 
