58e14b2f508db5cb09cfd85181b56edfe8a178b5
israakotb <israakotb55@gmail.com>
2022-07-18 16:49:56 +0200

    jjj

--- sample-front-end-project/main.js
+++ sample-front-end-project/main.js
@@ -1,16 +1,25 @@
-const cardImg = document.querySelector(".card-img");
-const cardName = document.querySelector(".card-name");
-const cardInfo = document.querySelector(".card-info");
+const cardImg = document.querySelector('.card-img')
+const cardName = document.querySelector('.card-name')
+const cardInfo = document.querySelector('.card-info')
 
 const cardData = [
-  { name: "M. Talaat Harb", img: "assets/mharb.webp", info: "TL of Cairo 6 from the Maiia Gestion team" },
+  {
+    name: 'M. Talaat Harb',
+    img: 'assets/mharb.webp',
+    info: 'TL of Cairo 6 from the Maiia Gestion team',
+  },
   
-];
+  {
+    name: 'Israa',
+    img: 'assets/mharb.webp',
+    info: 'Computer Science student',
+  }, 
+]
 
 function updateCard(id) {
-  const selectedData = cardData[id];
+  const selectedData = cardData[id]
 
-  cardImg.src = selectedData.img;
-  cardName.innerHTML = selectedData.name;
-  cardInfo.innerHTML = selectedData.info;
+  cardImg.src = selectedData.img
+  cardName.innerHTML = selectedData.name
+  cardInfo.innerHTML = selectedData.info
 }
