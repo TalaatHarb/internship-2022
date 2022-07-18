2c7406fb0fabb851e65dc176f91f18a7806ed618
something <some@thing.com>
2022-07-18 06:39:35 -0700

    mostafa's commit

--- sample-front-end-project/main.js
+++ sample-front-end-project/main.js
@@ -10,6 +10,9 @@ const cardData = [
   },
   { name: 'Mohamed Gado', img: 'assets/mharb.webp', info: 'Sadge' },
   { name: 'Hashem Yehia', img: 'assets/mharb.webp', info: 'Computer Science student' },
+  { name: 'Hashem Yehia'
+  , img: 'https://imgs.search.brave.com/uWySc1aj3JGNM8ymHEb8zjZMKRVBVQDG303z39YseLs/rs:fit:632:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5J/eDZYak1idUN2b3Ez/RVFOZ0pveUVRSGFG/aiZwaWQ9QXBp'
+  , info: 'Someone else' }
 ]
 
 function updateCard(id) {
