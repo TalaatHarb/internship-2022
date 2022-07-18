e51ab4d71e3f28daf152d0dfc30af70111667e04
Almoatasem <elmoatasemahmed@gmail.com>
2022-07-18 15:51:07 +0200

    hi world

--- sample-front-end-project/main.js
+++ sample-front-end-project/main.js
@@ -1,27 +1,17 @@
-const cardImg = document.querySelector('.card-img')
-const cardName = document.querySelector('.card-name')
-const cardInfo = document.querySelector('.card-info')
+const cardImg = document.querySelector(".card-img");
+const cardName = document.querySelector(".card-name");
+const cardInfo = document.querySelector(".card-info");
 
 const cardData = [
-  {
-    name: 'M. Talaat Harb',
-    img: 'assets/mharb.webp',
-    info: 'TL of Cairo 6 from the Maiia Gestion team',
-  },
-  { name: 'Mohamed Gado', img: 'assets/mharb.webp', info: 'Sadge' },
-  { name: 'Almoatasem Belah', img: 'assets/mharb.webp', info: 'QA intern' },
-  //heiii
-  { name: 'Hashem Yehia', img: 'assets/mharb.webp', info: 'Computer Science student' },
-  { name: 'MostafaEsmail3'
-  , img: 'https://imgs.search.brave.com/uWySc1aj3JGNM8ymHEb8zjZMKRVBVQDG303z39YseLs/rs:fit:632:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5J/eDZYak1idUN2b3Ez/RVFOZ0pveUVRSGFG/aiZwaWQ9QXBp'
-  , info: 'Someone else' }
-  
-]
+  { name: "M. Talaat Harb", img: "assets/mharb.webp", info: "TL of Cairo 6 from the Maiia Gestion team" },
+  { name: "Almoatasem Belah ", img: "assets/mharb.webp", info: "QA intern at Cegedim" },
+  //yarab
+];
 
 function updateCard(id) {
-  const selectedData = cardData[id]
+  const selectedData = cardData[id];
 
-  cardImg.src = selectedData.img
-  cardName.innerHTML = selectedData.name
-  cardInfo.innerHTML = selectedData.info
+  cardImg.src = selectedData.img;
+  cardName.innerHTML = selectedData.name;
+  cardInfo.innerHTML = selectedData.info;
 }
