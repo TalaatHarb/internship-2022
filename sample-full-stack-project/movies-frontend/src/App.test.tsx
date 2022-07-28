960ce353cc0c9cd7fabab1c1652165cd3aaebedb
Mohamed Talaat Harb <mohamed.harb@cegedim.com>
2022-07-28 13:24:38 +0200

    added some front-end work for the full stack sample

--- sample-full-stack-project/movies-frontend/src/App.test.tsx
+++ sample-full-stack-project/movies-frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Part of the home page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const partOfHomePage = screen.getByText(/Popular/i);
+  expect(partOfHomePage).toBeInTheDocument();
 });
--- sample-full-stack-project/movies-frontend/src/App.tsx
+++ sample-full-stack-project/movies-frontend/src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes
+} from "react-router-dom";
+import './App.scss';
+import Home from './pages/Home';
+import MovieDetails from './pages/MovieDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route
+            path="*"
+            element={<Navigate to="/movies" replace />}
+          />
+          <Route path="movies" element={<Home />} />
+          <Route path="movies/:id" element={<MovieDetails />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
